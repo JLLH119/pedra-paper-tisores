61f9e024f8bd987318b49890f46238f75c188741
JLLH119 <jlope119@xtec.cat>
2024-06-21 12:21:41 +0200

    Fitxers inicials per al projecte MakeCode

---
+++ main.py
@@ -0,0 +1,34 @@
+hand = 0
+
+def on_gesture_shake():
+    global hand
+    hand = randint(1, 3)
+    if hand == 1:
+        basic.show_leds("""
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            """)
+    elif hand == 2:
+        basic.show_leds("""
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            """)
+    else:
+        basic.show_leds("""
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            """)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
